2c5fc43df796d0e520d41374a95dc6f067b1fa4d
AhmadDaghameen <adaghameen@outlook.com>
2023-09-24 18:58:19 +0300

    add allure

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "cypress";
-
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 export default defineConfig({
   projectId: 'agsfgi',
   e2e: {
     setupNodeEvents(on, config) {
+      allureWriter(on, config);
       // implement node event listeners here
     },
   },

c8fe68260f9d12f1509bdca9ac45082318d2fc1a
AhmadDaghameen <adaghameen@outlook.com>
2023-09-30 14:50:02 +0300

    select span

---
+++ cypress/e2e/page-objects/Selector.ts
@@ -0,0 +1,8 @@
+class Selector{
+
+ SelectSpan(){
+    return cy.get('.orangehrm-attendance-card-bar').contains('span','today');
+}
+
+    
+}
---
+++ cypress/e2e/specs/Selector.spec.cy.ts
@@ -0,0 +1,28 @@
+
+import LoginPage from "../page-objects/LoginPage"
+
+const loginObj:LoginPage = new LoginPage();
+
+const Selectorobj: Selector = new Selector();
+
+describe('Login to the Home page', () => { 
+
+   
+    beforeEach(function()
+    {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+        loginObj.login('Admin', 'admin123')
+    })
+
+    it('get Time from Punch time',() => { 
+
+        Selectorobj.SelectSpan().then((z) => {
+            console.log(z);
+
+        });
+
+    });
+
+    
+
+})
